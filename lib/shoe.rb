416393035714ff1b8f4e83e3bea9b65063247079
Tabassum Hassan <tiram819@gmail.com>
2020-03-17 03:48:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,22 +1,4 @@
-class Shoe
-end
 
-class Shoe
-  def initialize(brand)
-    @brand = brand
-end
-
-def brand
-  @brand
-end 
-
-def color=(color)
-  @color = color
-end 
-  
-  def color
-    @color
-  end
 
 
 end
